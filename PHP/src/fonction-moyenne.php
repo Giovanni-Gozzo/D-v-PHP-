33097ea8c5ad659056695f7307a105abc3b9a802
Giovanni <gio.gozzo84@gmail.com>
2022-10-28 23:01:35 +0200

    Comit 1

--- PHP/src/fonction-moyenne.php
+++ PHP/src/fonction-moyenne.php
@@ -1,11 +1,15 @@
 <?php
 function moyenne(array $t): float{
-    $moy=0;
-    $i=0;
-    foreach($t as $n){
-        $moy=$moy+$n;
+    $moy = 0;
+    $i = 0;
+    foreach ($t as $n) {
+        $moy = $moy + $n;
         ++$i;
     }
-    $moy=$moy/$i;
+    $moy = $moy / $i;
     return $moy;
 }
+///*
+//Donnée: un tableau rempli seulement avec des entiers
+//Résulats: renvoie un entier qui correspond a la moyenne des entiers se trouvant dans le tableau.
+///*
