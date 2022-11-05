8bf9d848e453b3db0dc7c91a862c897704596eac
Giovanni <gio.gozzo84@gmail.com>
2022-11-05 13:18:48 +0100

    Comit

--- PHP/TD2/script-nombre-de-le.php
+++ PHP/TD2/script-nombre-de-le.php
@@ -1,16 +1,20 @@
 <?php
 $a=0;
 $b=0;
+$v=0;
 while($a!="."){
     $a=fgetc(STDIN);
-    if($a=="l"){
-        $a=fgetc(STDIN);
-        while($a=="l"){
-            $a=fgetc(STDIN);
+    if ($v==1) {
+        if ($a=="e"){
+            $b+=1;
         }
-        if($a=="e"){
-            $b=$b+1;
-        }
+        $v=0;
+    }
+    if ($a=="l"){
+        $v=1;
+    }
+    if ($a==" "){
+        $v=0;
     }
 }
 echo("***$b***");
