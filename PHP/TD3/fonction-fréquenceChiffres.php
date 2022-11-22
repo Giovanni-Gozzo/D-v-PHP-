1cb9c843d7997d4444fb302bd42f8568cc3343c6
Giovanni <gio.gozzo84@gmail.com>
2022-11-22 11:52:36 +0100

    Comit

--- PHP/TD3/fonction-fréquenceChiffres.php
+++ PHP/TD3/fonction-fréquenceChiffres.php
@@ -6,11 +6,9 @@ function fréquenceChiffres(int $n): array{
         $n=$n/10;
         $tab[$x]+=1;
     }
-    return$tab;
+    return $tab;
 }
-///*
-//Donnée:un entier naturel
-//Résulats:renvoie un tableau de longueur 10 constitué d'entier naturel ou nul qui correspondra
-//         a la fréquence d'un chiffre dans l'entier donnée.Le premier nombre du tableau correspondra
-//         au nombre de zeros le 2e nombre au nombre de 1 ... jusque 9.
-///*
+/*
+*Donnée:un entier naturel
+*Résulats:renvoie un tableau de longueur 10 constitué d'entier naturel ou nul qui correspondra a la fréquence d'un chiffre dans l'entier donnée.Le premier nombre du tableau correspondra         au nombre de zeros le 2e nombre au nombre de 1 ... jusque 9.
+*/
